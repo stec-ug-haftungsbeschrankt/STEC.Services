3378b3329b7d91d91a66b5c78f762dbe4ddfd281
Stefan Schick <stefan.schick@stecug.de>
2020-12-21 12:36:38 +0100

    Add Cache Key Factory and base class for context

---
+++ STEC.Services.UserTenants/ApplicationCacheKeyFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace STEC.Services.UserTenants
+{
+    public class ApplicationCacheKeyFactory : IModelCacheKeyFactory
+    {
+        public object Create(DbContext context)
+        {
+            if (context is ApplicationDbContextBase)
+            {
+                ApplicationDbContextBase myContext = context as ApplicationDbContextBase;
+                return (context.GetType(), myContext.Schema);
+            }
+            return context.GetType();
+        }
+    }
+}
---
+++ STEC.Services.UserTenants/ApplicationDbContextBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace STEC.Services.UserTenants
+{
+    public class ApplicationDbContextBase : DbContext
+    {
+        protected ITenantProvider _tenantProvider;
+
+        public string Schema
+        {
+            get
+            {
+                return _tenantProvider.GetTenantSchema().Result;
+            }
+        }
+
+        public ApplicationDbContextBase(DbContextOptions<ApplicationDbContextBase> options, ITenantProvider tenantProvider) : base(options)
+        {
+            _tenantProvider = tenantProvider;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Used for Schema Switch
+            optionsBuilder.ReplaceService<IModelCacheKeyFactory, ApplicationCacheKeyFactory>();
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
