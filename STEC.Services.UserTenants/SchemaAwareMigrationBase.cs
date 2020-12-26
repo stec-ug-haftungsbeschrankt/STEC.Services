2608d55ec29e3866231b022d2fa08e069dee127c
Stefan Schick <stefan.schick@stecug.de>
2020-12-26 12:41:49 +0100

    Add Base class for migrations

---
+++ STEC.Services.UserTenants/SchemaAwareMigrationBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace STEC.Services.UserTenants
+{
+    public partial class SchemaAwareMigrationBase : Migration
+    {
+        protected string _schema;
+
+
+        public SchemaAwareMigrationBase()
+        {
+            _schema = "public";
+        }
+
+
+        public SchemaAwareMigrationBase(ApplicationDbContextBase _context)
+        {
+            _schema = _context.Schema ?? throw new ArgumentNullException(nameof(_context));
+        }
+
+        protected override void Up([NotNullAttribute] MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down([NotNullAttribute] MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
